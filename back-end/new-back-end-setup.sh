52e7f2708169ef73b07d9a973c2b3c201ed830b0
LuizNach <luiz.nach@gmail.com>
2021-04-01 03:59:38 -0300

    BE typescript project init

---
+++ back-end/new-back-end-setup.sh
@@ -0,0 +1,10 @@
+echo "=> LOG: Initialize package manager"
+yarn init -y
+echo "=> LOG: Add Express and types as dev dependency"
+yarn add express
+yarn add @types/express -D
+echo "=> LOG: Add Typescript as dev dependecy"
+yarn add typescript -D
+echo "=> LOG: Initialize typescript project - Add ts config json file"
+yarn tsc --init
+# to convert automatically ts files into js run: yarn tsc
