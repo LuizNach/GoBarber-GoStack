a13ea849eb1f615a0642b71ff162c18b117354d4
LuizNach <luiz.nach@gmail.com>
2021-04-04 22:59:37 -0300

    BE set package.json to attach vscode debugger

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,26 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
-const port = '3333';
+const port = 3333;
+app.use(express.json())
 
 
 app.get('/', (request, response) => {
     return response.json({message: 'Hello'})
 });
+
+app.post('/', (request, response) => {
+
+    const { email, name } = request.body;
+    
+    const user = {
+        name,
+        email
+    }
+
+    return response.json(user);
+});
+
+
 
 app.listen(port, () => console.log(`🚀️ Server started on port: ${port}!`));
